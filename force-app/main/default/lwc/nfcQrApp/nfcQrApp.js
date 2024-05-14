711e0a74c31b5d29167b549694a2163c8191caec
Marian Pisotskyi <marianpisotskyi@Marians-MacBook-Pro.local>
2024-05-14 17:52:07 +0300

    Main app update with the third-party data

--- force-app/main/default/lwc/nfcQrApp/nfcQrApp.js
+++ force-app/main/default/lwc/nfcQrApp/nfcQrApp.js
@@ -1,12 +1,40 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
+import getExchangeRates from '@salesforce/apex/NfcQrAppController.getExchangeRates';
+
+const COLUMNS = [
+    { label: 'Currency', fieldName: 'txt'},
+    { label: 'Code', fieldName: 'cc'},
+    { label: 'Rate', fieldName: 'rate'}
+];
 
 export default class NfcQrApp extends LightningElement {
     showMainMenu = false;
     showNfcReader = false;
     showQrScanner = false;
 
+    columns = COLUMNS;
+
+    @track exchangeRates;
+
+    get showNavigationButton() {
+        return this.showNfcReader || this.showQrScanner;
+    }
+
+    get showExchangeRates() {
+        return this.showMainMenu && this.exchangeRates;
+    }
+
     connectedCallback() {
         this.showMainMenu = true;
+
+        this.getExchangeRates();
+    }
+
+    getExchangeRates() {
+        getExchangeRates()
+            .then((result) => {
+                this.exchangeRates = JSON.parse(result).slice(0, 5);
+            }) 
     }
 
     handleNfcReaderClick() {
@@ -17,5 +45,12 @@ export default class NfcQrApp extends LightningElement {
     handleQrScannerClick() {
         this.showMainMenu = false;
         this.showQrScanner = true;
+    }
+
+    handleGoToMainMenu() {
+        this.showNfcReader = false;
+        this.showQrScanner = false;
+
+        this.showMainMenu = true;
     }
 }
